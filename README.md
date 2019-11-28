59dfdbe6972ab853295607e3850b3650e9c14449
Shadoworker <bibsunique@gmail.com>
2019-11-28 16:20:53 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Egattes
-egattes game
+# godot-simple-drag-and-drop
+A simple drag and drop system for godot games
---
+++ actor.gd
@@ -0,0 +1,38 @@
+extends Sprite
+
+var mouseIn = false
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _process(delta):
+	# You need to create a action caleld "click"(Left mouse click) in the project input map.
+	# Você precisa criar uma ação chamada click(botão esquerdo do mouse) no input map do seu projeto
+	if(mouseIn && Input.is_action_pressed("click")):
+		set_position(get_viewport().get_mouse_position())
+	pass
+
+
+#func _on_Area2D_mouse_entered():
+#	mouseIn = true
+#	pass # replace with function body
+#
+#
+#func _on_Area2D_mouse_exited():
+#	mouseIn = false
+##	print_debug("launched")
+#	pass # replace with function body
+
+
+func _on_Button_button_down():
+	mouseIn = true
+	print_debug("pinched")
+	pass # Replace with function body.
+
+
+func _on_Button_button_up():
+	mouseIn = false
+	print_debug("launched")
+	pass # Replace with function body.
